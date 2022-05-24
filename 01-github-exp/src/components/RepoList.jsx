5fd15fcaacc1661c130ea7ea28b3d80536f5df94
Jefferson <jeffmonteiro61@icloud.com>
2022-05-23 23:04:02 -0300

    chore:componentsGitHub

---
+++ 01-github-exp/src/components/RepoList.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+
+export function RepoList(){
+    return (
+        <section className="repository-list">
+            <h1>Lista de Repositórios</h1>
+
+            <ul>
+                <li>
+                    <strong>unform</strong>
+                    <p>Forms in React</p>
+
+                    <a href="https://github.com/">
+                        Acessar Repositórios
+                    </a>
+                </li>
+            </ul>
+        </section>
+    );
+}
