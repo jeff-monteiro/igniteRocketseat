4af32a6e646305130841b062df59d62a8a1c99dd
Jefferson Monteiro <jeffmonteiro61@icloud.com>
2022-05-29 14:40:36 -0300

    repoFollower

---
+++ 01-github-exp/src/components/RepoFollower.jsx
@@ -0,0 +1,13 @@
+export function RepoFollower() {
+    return (
+        <li>
+            <strong>unform</strong>
+            <p>Forms in React</p>
+
+            <a href="https://github.com/">
+                Acessar Repositórios
+            </a>
+        </li>
+
+    );
+}
