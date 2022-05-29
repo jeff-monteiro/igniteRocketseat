355d83bad11b4a3bd251b59d41a0eb07150afac3
Jefferson Monteiro <jeffmonteiro61@icloud.com>
2022-05-28 21:35:33 -0300

    propsInReact

--- 01-github-exp/src/components/RepoList.jsx
+++ 01-github-exp/src/components/RepoList.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { RepoItem } from './RepoItem';
 
-const repositoryName = 'unform'
+//const repositoryName = 'unform'
 
 export function RepoList(){
     return (
@@ -9,25 +9,9 @@ export function RepoList(){
             <h1>Lista de Repositórios</h1>
 
             <ul>
-                <li>
-                    {RepoItem}
-                </li>
-                <li>
-                    <strong>unform</strong>
-                    <p>Forms in React</p>
-
-                    <a href="https://github.com/">
-                        Acessar Repositórios
-                    </a>
-                </li>
-                <li>
-                    <strong>unform</strong>
-                    <p>Forms in React</p>
-
-                    <a href="https://github.com/">
-                        Acessar Repositórios
-                    </a>
-                </li>
+                <RepoItem />
+                <RepoItem />
+                <RepoItem />
             </ul>
         </section>
     );
