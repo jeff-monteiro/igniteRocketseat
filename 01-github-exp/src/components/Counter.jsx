ccbe35660083868b2ce1b1e9e672bc76632cf893
Jefferson Monteiro <jeffmonteiro61@icloud.com>
2022-06-19 23:26:44 -0300

    chore:counterClick

--- 01-github-exp/src/components/Counter.jsx
+++ 01-github-exp/src/components/Counter.jsx
@@ -1,8 +1,19 @@
 export function Counter(){
+
+    let counter = 0;
+
+    function increment(){
+        counter += 1;
+        //console.log('Incrementing')
+    }
+
+
     return (
         <div>
             <h2>0</h2>
-            <button type = "button">Increment</button>
+            <button type = "button" onClick = {increment}>
+            Increment
+            </button>
         </div>
     )
 }
