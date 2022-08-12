95550656bf052dc4604509417747efed5cef4855
Jefferson Monteiro <jeffmonteiro61@icloud.com>
2022-08-11 22:53:38 -0300

    updatingSection

--- 01-github-exp/src/components/RepoList.jsx
+++ 01-github-exp/src/components/RepoList.jsx
@@ -24,7 +24,7 @@ export function RepoList(){
 
             <ul>
                 {repositories.map(repository => {
-                    return <RepoItem key={repository.name}    rrrepository ={repository} />
+                    return <RepoItem key={repository.name} repository ={repository} />
                 })}
             </ul>
         </section>
